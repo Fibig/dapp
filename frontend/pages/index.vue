b49bfcb8b53ce5f84fcb21cd9265164563459584
Fabian Gesswagner <fibig2005@gmail.com>
2024-02-28 10:22:54 +0100

    Refactor MetaMask connection and verification process

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -17,15 +17,12 @@ const accounts = ref([]);
 const state = reactive({
     connected: false,
     verified: false,
+    verificationError: false
 });
 
 // SOCKET
 const socketUrl = "http://localhost:4000";
 const socket = io(socketUrl);
-
-socket.on("connect", () => {
-    state.connected = true;
-});
 
 // LIFE CYCLE
 onMounted(() => {
@@ -46,9 +43,11 @@ async function metaMaskConnect() {
             return;
         }
 
-        socket.emit("setWallet", accounts.value[0]);
+        socket.on("connect", () => {
+            state.connected = true;
+        });
 
-        buyVerification();
+        socket.emit("setWallet", accounts.value[0]);
     }
 }
 
@@ -57,18 +56,22 @@ function disconnectSocket() {
 }
 
 async function buyVerification() {
-    const response = await window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [
-            {
-                from: accounts.value[0],
-                to: receiverAddress,
-                value: '0x0'
-            }
-        ]
-    })
-
-    console.log(response)
+    try {
+        const response = await window.ethereum.request({
+            method: 'eth_sendTransaction',
+            params: [
+                {
+                    from: accounts.value[0],
+                    to: receiverAddress,
+                    value: '0x0'
+                }
+            ]
+        })
+        state.verified = true;
+        state.verificationError = false;
+    } catch (error) {
+        state.verificationError = true;
+    }
 }
 
 </script>
@@ -77,8 +80,12 @@ async function buyVerification() {
     <div>
         <p v-if="!isMetaMaskInstalled">Please install MetaMask</p>
         <p v-if="accounts.length === 0">Please connect a wallet</p>
-        <p v-if="state.connected">Connected</p>
+        <p v-if="state.connected">Connected: {{ accounts[0] }}</p>
         <p v-if="state.verified">Verified</p>
-        <p v-else>Buy Verification</p>
+        <div v-else>
+            <p>Buy Verification to proceed</p>
+            <button @click="buyVerification">Buy Verification</button>
+            <p v-if="state.verificationError">Error during Verification process</p>
+        </div>
     </div>
 </template>
